f086c5ba3273ebe5ddb32ef4335a4b696aae5695
Іра <iryna.nazar@ukma.edu.ua>
2023-01-16 17:49:24 -0500

    start stylist page

---
+++ src/pages/Stylists/Stylists.js
@@ -0,0 +1,96 @@
+export const StylistHair = [
+	{
+		name: 'JULIA GAFFNEY',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63b61ccf90d5725ecaaf456d/1672879311550/ANA_MONTES_DE_OCA_CLR_2.jpg',
+		services: 'SERVICES: Color, Cutting, Styling, Smoothing Systems',
+	},
+	{
+		name: 'RENO PREZIO',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/6399be9d161ed05edd5243af/1671020189270/RENO_PREZIO_CLR.jpg',
+		services: 'SERVICES: Haircuts, Color, Smoothing Treatments, and Updo’s',
+	},
+	{
+		name: 'MONICA DE LA ROSA',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/6399b3300b201a7f2b0c35bf/1671017265066/BRIANA_WAITE_CLR.jpg',
+		services:
+			'SERVICES: Color, Extensions, Cutting, Smoothing Treatments (Brazilian and Keratin), Blowouts',
+	},
+	{
+		name: 'MONIKA VALADEZ',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63b5f5cf425b6851330c44fd/1672869327426/Kelsey_Norton_CLR_2.jpg',
+		services: 'SERVICES: Hair extensions and color',
+	},
+	{
+		name: 'BRIANA WAITE',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/6399b8cbdff35264be3ba1d3/1671018699457/CHELCIE_LEAL_CLR.jpg',
+		services: 'SERVICES: Haircuts, Color, Smoothing Treatments, Updo’s',
+	},
+];
+
+export const NailMaster = [
+	{
+		name: 'RACHAEL HOANG',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63a234e5d673c5048b00af20/1671574757848/RACHAEL_HOANG_CLR.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicure, Pedicures',
+	},
+
+	{
+		name: 'JESSICA FLORES',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/639978684094aa6232a0bebc/1671002217112/ALI_MCKINNEY_CLR_2.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicures, Pedicures',
+	},
+	{
+		name: 'RACHAEL HOANG',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63a2403a7c226a4906563ec8/1671577658375/JAYE_LYNN_CLR.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicures, Pedicures',
+	},
+
+	{
+		name: 'JESSICA FLORES',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63a246cb01f6f56d8fe4050d/1671579339674/ARIELLE_GARCIA_CLR_2.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicure',
+	},
+];
+
+export const MakeupStylist = [
+	{
+		name: 'BRIANA WAITE',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/6399b3300b201a7f2b0c35bf/1671017265066/BRIANA_WAITE_CLR.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicure, Pedicures',
+	},
+
+	{
+		name: 'NICO CAZAREZ',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/639c6f26230d993eb9065878/1671196455116/NICO_CAZAREZ_CLR_2.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicures, Pedicures',
+	},
+	{
+		name: 'STEPHANIE MONTANO',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63a23ecc31178a626b0d98c0/1671577293083/STEPHANIE_MONTANA_CLR.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicures, Pedicures',
+	},
+
+	{
+		name: 'JOSE MONTES DE OCA',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63a236162c437c111961cc57/1671575062484/JOSE_MONTES_DE_OCA_CLR.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicure',
+	},
+	{
+		name: 'ARIELLE GARCIA',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/639c5b976fe46b398fb352f1/1671191448218/MONIKA_VALDEZ_CLR.jpg',
+		services: 'SERVICES: Acrylic, Gels, Polishing, Manicure',
+	},
+];
+
+export const WaxMaster = [
+	{
+		name: 'CEDAR FLYTER',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/6399c05edede5e564c7b5fcd/1671020639356/JULIA_GAFFNEY_CLR.jpg',
+		services: 'SERVICES: Face, Full Body',
+	},
+	{
+		name: 'CHELCIE LEAL',
+		img: 'https://static1.squarespace.com/static/62f5f1391c1114796efe189c/t/63a246cb01f6f56d8fe4050d/1671579339674/ARIELLE_GARCIA_CLR_2.jpg',
+		services: 'SERVICES: Lower Body, Upper Body, Bikini',
+	},
+];
--- src/router.js
+++ src/router.js
@@ -1,21 +1,21 @@
-import {About} from "./pages/About/AboutPage";
+import { About } from './pages/About/AboutPage';
 import App from './App';
-import {
-    createBrowserRouter,
-} from "react-router-dom";
-
+import { createBrowserRouter } from 'react-router-dom';
+import { StylistsPage } from './pages/Stylists/StylistsPage';
 
 export const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <App />,
-        children: [
-            {
-                path: "about",
-                element: <About />,
-            },
-        ]
-    },
-
-
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: 'about',
+				element: <About />,
+			},
+			{
+				path: 'stylists',
+				element: <StylistsPage />,
+			},
+		],
+	},
 ]);
